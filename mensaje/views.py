9bef98da65b776dc894cf9fcae5a46bbb7259a10
AlbertCardona <albert.cardona.03@gmail.com>
2023-03-06 14:04:57 -0600

    Cambios estandar de código

--- mensaje/views.py
+++ mensaje/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def hola(request):
     return render(request, 'hola.html')
 
-def hola2(request):
-    return render(request,'hola.html')
 
+def hola2(request):
+    return render(request, 'hola.html')
